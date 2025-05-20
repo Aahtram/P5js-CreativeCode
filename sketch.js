27b8cb7de063243b4efe581b4f6fe25591cd722e
Aahtram <160880397+Aahtram@users.noreply.github.com>
2025-05-20 11:50:51 +0200

    Upload manuellement

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400); 
+  noStroke();
+}
+
+function draw() {
+  background(255);  
+  
+  
+  
+  fill(255, 0, 0, 140); 
+  ellipse(130, 150, 200, 200); 
+
+ 
+  fill(0, 255, 0, 140);  
+  ellipse(270, 150, 200, 200); 
+  
+  
+  fill(0, 0, 255, 140); 
+  ellipse(200, 250, 200, 200); 
+}
